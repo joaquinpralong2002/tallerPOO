3f72b72533e2758d6f945e2970c81640072a226c
joaquinpralong2002 <joaquinpralong2002@users.noreply.github.com>
2023-10-02 23:30:11 -0300

    Creación de clases e implementación de algunos métodos. Versión Gastón.

--- src/main/java/model/Paciente.java
+++ src/main/java/model/Paciente.java
@@ -14,7 +14,10 @@ import java.util.List;
 public class Paciente extends Persona{
     private Long idPaciente;
     private Persona personaContacto;
+
     private List<ResultadoDiagnostico> resultadosDiagnosticos;
+    private List<RegistroEntrada> registrosEntradas;
+    private List<Registro> registros;
 
     public Paciente(String nombreApellido, LocalDate fechaNacimiento, String domicilio, int DNI,
                     int telefonoFijo, long telefonoCelular, EstadoCivil estadoCivil, String correo,
@@ -31,9 +34,33 @@ public class Paciente extends Persona{
         this.resultadosDiagnosticos = resultadosDiagnosticos;
     }
 
-    public void agregarResultadoDiagnostico(ResultadoDiagnostico r){
-        resultadosDiagnosticos.add(r);
+    public Paciente(String nombreApellido, LocalDate fechaNacimiento, String domicilio, int DNI,
+                    int telefonoFijo, long telefonoCelular, EstadoCivil estadoCivil, String correo,
+                    Persona personaContacto, List<ResultadoDiagnostico> resultadosDiagnosticos, List<RegistroEntrada> registrosEntradas) {
+        super(nombreApellido, fechaNacimiento, domicilio, DNI, telefonoFijo, telefonoCelular, estadoCivil, correo);
+        this.personaContacto = personaContacto;
+        this.resultadosDiagnosticos = resultadosDiagnosticos;
+        this.registrosEntradas = registrosEntradas;
     }
 
+    public Paciente(String nombreApellido, LocalDate fechaNacimiento, String domicilio, int DNI,
+                    int telefonoFijo, long telefonoCelular, EstadoCivil estadoCivil, String correo,
+                    Persona personaContacto, List<ResultadoDiagnostico> resultadosDiagnosticos,
+                    List<RegistroEntrada> registrosEntradas, List<Registro> registros) {
+        super(nombreApellido, fechaNacimiento, domicilio, DNI, telefonoFijo, telefonoCelular, estadoCivil, correo);
+        this.personaContacto = personaContacto;
+        this.resultadosDiagnosticos = resultadosDiagnosticos;
+        this.registrosEntradas = registrosEntradas;
+        this.registros = registros;
+        this.registros = registros;
+    }
+
+    public void agregarResultadoDiagnostico(ResultadoDiagnostico resultado){
+        resultadosDiagnosticos.add(resultado);
+    }
+
+    public void agregarRegistroEntrada(RegistroEntrada registro){
+        registrosEntradas.add(registro);
+    }
 
 }
