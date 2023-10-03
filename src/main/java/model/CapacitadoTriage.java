61813f0195f7435ac54869dd2889fbc935a76062
joaquinpralong2002 <joaquinpralong2002@users.noreply.github.com>
2023-10-02 23:31:39 -0300

    Creación de clases e implementación de algunos métodos. Versión Emanuel.

---
+++ src/main/java/model/CapacitadoTriage.java
@@ -0,0 +1,20 @@
+package model;
+
+import model.Enum.ColorTriage;
+
+public interface CapacitadoTriage {
+
+    /**
+     * Metodo que realiza un Triage en base al paciente otorgado por paramentro
+     * @param p de tipo Paciente
+     * @return Devuelve un triage de tipo ColorTriage
+     */
+    public ColorTriage realizarTriage(Paciente p);
+
+    /**
+     * Metodo que modifica el color de un triage ya realizado
+     * @param p de tipo Paciente, sirve para saber el triage que se le va a cambiar el color
+     * @param motivo De tipo String, sirve para almacenar el motivo por el cual el color del triage fue modificado
+     */
+    public void cambiarColorTriage(Paciente p, String motivo);
+}
