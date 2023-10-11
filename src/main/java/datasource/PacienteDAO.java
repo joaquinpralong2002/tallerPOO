84d3b3b815336e489ebe58447234605ee1ce0181
Emanuel Bozzo <emabozzo02@gmail.com>
2023-10-11 20:03:28 -0300

    Changes by gasto

--- src/main/java/datasource/PacienteDAO.java
+++ src/main/java/datasource/PacienteDAO.java
@@ -1,32 +1,38 @@
 package datasource;
+import datasource.interfaces.GenericoDAO;
 import model.*;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+
+import org.hibernate.*;
 import util.GlobalSessionFactory;
 
-public class PacienteDAO {
+import java.util.List;
+
+public class PacienteDAO implements GenericoDAO<Paciente> {
     private SessionFactory sessionFactory;
 
     public PacienteDAO(){
         this.sessionFactory = GlobalSessionFactory.getSessionFactory();
     }
-    
-    public void agregarPaciente(Paciente paciente){
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(paciente);
-        transaction.commit();
-        session.close();
-    }
 
-    public Paciente obtenerPaciente(Long id) {
+    //************** BASICOS DE PACIENTE*******************
+    @Override
+    public Paciente obtener(Long id) {
         Session session = sessionFactory.openSession();
         Paciente paciente = session.get(Paciente.class, id);
         session.close();
         return paciente;
     }
+    @Override
+    public List<Paciente> obtenerTodos() {
+        Session session = sessionFactory.openSession();
+        String query = "SELECT paciente FROM Paciente paciente";
+        List<Paciente> paciente = session.createQuery(query).getResultList();
+        session.close();
+        return paciente;
+    }
+    //********************************************************************
+
 
 
 }
