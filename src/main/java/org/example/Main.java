05ce450765ccb58e3cc9ce312557874c380a85d2
joaquinpralong2002 <joaquinpralong2002@users.noreply.github.com>
2023-10-12 18:55:42 -0300

    Changes by Jesús Ksa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,58 +16,6 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        //objetos ramdom pa probar
-        Asignacion asignacion1 = new Asignacion(LocalDate.now(), LocalTime.now());
-
-
-        //Pensa como: el funcionario entra al sistema, llega una persona y en la ventana donde crea el registro
-        FuncionarioAdministrativo f1 = new FuncionarioAdministrativo();
-
-        //toma los datos del paciente
-        Paciente p1 = new Paciente("juancito flores",
-                LocalDate.of(2000,
-                        8,11),
-                "asdas",
-                12345678,
-                0,
-                0,
-                EstadoCivil.Soltero,
-                "dasfafvfcas",
-                null);
-
-        //pone los datos en la ventana, los datos del registro tambien (fecha,hora y descripcion), y le da al boton crear registro
-        f1.RealizarRegistroEntrada(p1,"dolor cabeza");
-
-        //Ahora un medico o enfermero X lo llama para realizar el triage, ya esta en la ventana del programa para realizar el triage
-        Universidad universidad = new Universidad("Uner");
-        Especialidad ep = new Especialidad("Cardiologia Clinica",LocalDate.of(2014,5,11));
-        BoxAtencion box1 = new BoxAtencion(1,2,true);
-        ep.setUniversidad(universidad);
-        Rol rol = new Rol("Salud");
-        Usuario usuario = new Usuario("usuario","Pepito01@",);
-        rol.setUsuarios(List.of(usuario));
-        Medico m1 = new Medico("Pepito Moreno",
-                LocalDate.of(1987,2,20),
-                "sasffas",
-                12345679,
-                0,
-                0,
-                EstadoCivil.Casado,
-                "dsac",
-                new Sector("Cardiologia"),
-                "1a2b3c4d5e");
-        m1.agregarEspecializacion(ep);
-
-        System.out.println(m1);
-
-        //empieza a hacerles las preguntas y seleccionarlas en la ventana y le da al boton de crear triage
-        m1.realizarTriage(p1.getRegistrosEntradas().get(0));
-        //si el que realizo el triage cambia el resultado setear el motivo y el color nuevo
-
-        //ponele q el mismo medico va a atender ese paciente, el medico vera una lista de pacientes en la ventana y va a decidir atenderlo
-        m1.atenderPaciente(p1, box1,p1.getRegistrosEntradas().get(0));
-        */
 
         //Se inicializa el Session Factory de la clase GlobalSessionFactory.
         GlobalSessionFactory init = new GlobalSessionFactory();
