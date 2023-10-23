6cbbe6b3ae7f0a55a483de0ff12ff1b0239d66d4
jesusksa <72816948+jesusksa@users.noreply.github.com>
2023-10-23 14:53:05 -0300

    desarrollo sobre el administrados de sistemas, creada ventana y controller para crear usuarios

---
+++ src/main/java/controllers/CrearUsuarioController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import datasource.FuncionarioDAO;
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CrearUsuarioController {
+    public TextField txtNombUsu;
+    public PasswordField txtPassw;
+    public CheckBox cbFuncionario;
+    public CheckBox cbInformatico;
+    public CheckBox cbMedico;
+    public CheckBox cbEnfermero;
+    public TextField txtDniFunc;
+    public Label lbNomFunc;
+    public Label lbApeFunc;
+    public Label lbDniFunc;
+    public Label lbSectorFunc;
+
+    public void Volver(ActionEvent actionEvent) {
+    }
+
+    public void BuscarFuncionario(ActionEvent actionEvent) {
+    }
+
+    public void CrearUsuario(ActionEvent actionEvent) {
+    }
+}
