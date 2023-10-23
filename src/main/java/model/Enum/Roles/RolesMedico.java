c50fbed65d8c3f5abf47a16a9bdcb0ef1f520343
joaquinpralong2002 <joaquinpralong2002@users.noreply.github.com>
2023-10-23 12:22:33 -0300

    -Creadas enumeraciones con roles genéricos para funcionarios, enfermeros y médicos. -Implementado control que oculte el botón de realizar triage si el médico o enfermero no posee el rol "Triage".

---
+++ src/main/java/model/Enum/Roles/RolesMedico.java
@@ -0,0 +1,6 @@
+package model.Enum.Roles;
+
+public enum RolesMedico {
+    Triage, MedicoEmergencias, MedicoGeneral, MedicoEspecialista, Cirujano, Anestesiologo, Radiologo,
+    Psiquiatra, MedicoCuidadosIntensivos, Geriatra, Pediatra, MedicoAtencionPrimaria;
+}
