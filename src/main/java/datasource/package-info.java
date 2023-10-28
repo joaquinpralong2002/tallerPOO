673810228931fe078906d44d632a65fd7bff4de6
jesusksa <72816948+jesusksa@users.noreply.github.com>
2023-10-28 19:14:28 -0300

    documentacion de packgeinfo

---
+++ src/main/java/datasource/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Este paquete "datasource" contiene los objetos de acceso a datos (DAO) que se utilizan para interactuar con la base de datos
+ * en la aplicación. Los DAO son responsables de realizar operaciones de lectura, escritura y actualización en la base de datos.
+ *
+ * En este paquete, cada clase de DAO se encarga de un tipo específico de entidad o tabla en la base de datos. Proporcionan
+ * métodos para realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) en registros de la base de datos relacionados
+ * con esa entidad.
+ *
+ * Además de interactuar con la base de datos, algunos DAO incluyen métodos personalizados para realizar consultas
+ * complejas, agregaciones o cualquier otra operación específica que sea relevante para la entidad correspondiente.
+ *
+ * Los DAO en este paquete se utilizan en conjunto con las clases de modelo que representan las entidades de la base de datos.
+ */
+package datasource;
